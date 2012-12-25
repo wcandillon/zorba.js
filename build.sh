9ba1126aa324ae0996035df4f10fc16c9ec9e59e
William Candillon <wcandillon@28.io>
2012-12-25 16:09:25 +0100

    Add main cpp file.

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ ${EMSCRIPTEN}/emmake make $1
 cp src/libzorba_simplestore.so ${PWD}/../../system/lib/
 cp -R include/zorba ${PWD}/../../system/include/
 cp -R ../include/zorba/* ${PWD}/../../system/include/zorba
-
+cd ../..
+JAVA_HEAP_SIZE=4096m ${EMSCRIPTEN}/em++ -O2 src/zorba.cpp -I${PWD}/system/include/ -L${PWD}/system/lib -lzorba_simplestore -o zorba.html
---
+++ src/zorba.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2006-2008 The FLWOR Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <sstream>
+
+#include <zorba/zorba.h>
+#include <zorba/store_manager.h>
+#include <zorba/serializer.h>
+#include <zorba/singleton_item_sequence.h>
+#include <zorba/zorba_exception.h>
+
+
+using namespace zorba;
+
+int
+runQuery(char* query)
+{
+  void* lStore = StoreManager::getStore();
+  Zorba *lZorba = Zorba::getInstance(lStore);
+
+  XQuery_t lQuery = lZorba->compileQuery(query);
+
+  std::cout << lQuery << std::endl;
+
+  lZorba->shutdown();
+  StoreManager::shutdownStore(lStore);
+
+  return 0;
+}
+
+int 
+main(int argc, char* argv[])
+{
+  return runQuery("1+1");
+}
