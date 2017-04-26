73092ad319f392560857289c2888e71bd7708ba2
Henri Manson <hfmanson@gmail.com>
2017-04-26 14:55:37 +0200

    zorba.js using WASM

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-JAVA_HEAP_SIZE=4096m ${EMSCRIPTEN}/em++ -O2 src/zorba.cpp -I${PWD}/system/include/ -L${PWD}/system/lib -lzorba_simplestore -o zorba.html
+JAVA_HEAP_SIZE=4096m ${EMSCRIPTEN}/em++ \
+	-s WASM=1 \
+	-O2 \
+	src/zorba.cpp \
+	-I${PWD}/system/include/ \
+	-L${PWD}/system/lib \
+	-lzorba_simplestore \
+	-o zorba.html
